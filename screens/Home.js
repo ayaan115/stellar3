5371db92a83764ead52df7c97c48e32962f38555
Hacker <93960319+ayaan115@users.noreply.github.com>
2023-04-16 14:33:52 +0530

    Update Home.js

--- screens/Home.js
+++ screens/Home.js
@@ -16,26 +16,29 @@ export default class HomeScreen extends Component {
         return (
             <View style={styles.container}>
                 <SafeAreaView style={styles.droidSafeArea}/>
-                <ImageBackground
-                source={{require("../assets/stars.gif")}}
-                style={styles.backgroundImage}></ImageBackground>
-                <View style={styles.titleBar}>
-                    <Image 
-                    source={{require("../assets/main-icon.png")}}
-                    style={{width: 150, height: 150}}
-                    ></Image>
-                    <Text style={styles.titleText}>Stellar</Text>
-                    <Text style={styles.titleText}>App</Text>
-                </View>
-                <TouchableOpacity
-                style={styles.routeCard}
-                onPress={() => this.props.navigation.navigate}
-                >
-                    <Text style={styles.routeText}>SpaceCrafts</Text>
-                    <Image
-                    source={require('../assets/space_crafts.png')}
-                    style={styles/routeImage}></Image>
-                </TouchableOpacity>
+                <ImageBackground source={{require("../assets/stars.gif")}} style={styles.backgroundImage}>
+                    <View style={styles.titleBar}>
+                        <Image source={{require("../assets/main-icon.png")}} style={{width: 150, height: 150}}></Image>
+                        <Text style={styles.titleText}>Stellar</Text>
+                        <Text style={styles.titleText}>App</Text>
+                    </View>
+
+                    <TouchableOpacity style={styles.routeCard}  onPress={() => this.props.navigation.navigate}>
+                        <Text style={styles.routeText}>SpaceCrafts</Text>
+                        <Image source={require('../assets/space_crafts.png')} style={styles/routeImage}></Image>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity style={styles.routeCard}  onPress={() => this.props.navigation.navigate}>
+                        <Text style={styles.routeText}>Star Map</Text>
+                        <Image source={require('../assets/atar_map.png')} style={styles/routeImage}></Image>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity style={styles.routeCard}  onPress={() => this.props.navigation.navigate}>
+                        <Text style={styles.routeText}>Daily Pictures</Text>
+                        <Image source={require('../assets/daily_pictures.png')} style={styles/routeImage}></Image>
+                    </TouchableOpacity>
+
+                </ImageBackground>
             </View>
         )
     }
